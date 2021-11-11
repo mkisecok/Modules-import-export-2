be8efc94989b54fa7fa7c002a6c0dc704c172d84
Mkisecok <mahmutkisecok@gmail.com>
2021-11-11 15:04:15 +0100

    Add percentageForm

--- modules/index.js
+++ modules/index.js
@@ -20,19 +20,25 @@ difference();
 calculateAspectRatio();
 fixRoundingErrors();
 
- 
-
- 
-
-
-
  const moduloForm=document.querySelector('#modulo-form');
- 
 
+ const percentageForm=document.querySelector('#percentage-form');
+
+
+ 
+// 1
  moduloForm.addEventListener('change', () => {
     moduloForm[2].value=modulo(moduloForm[0].value,moduloForm[1].value)
   
  });
+
+ //2
+ percentageForm.addEventListener('change', ()=>
+ {
+     percentageForm[2].value=percentage(percentageForm[0].value,percentageForm[1].value )
+ })
+
+
  
 //
  
